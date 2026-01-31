4b40d2eb3f12853bb49d40833b1c635b6fa7913f
Alinondze <alinakohut525@gmail.com>
2026-01-31 12:49:59 +0200

    Add Item classes and update SlashAnimInstance

--- Source/MyProject4/Public/Characters/SlashAnimInstance.h
+++ Source/MyProject4/Public/Characters/SlashAnimInstance.h
@@ -1,27 +1,27 @@
-
-
-#pragma once
-
-#include "CoreMinimal.h"
-#include "Animation/AnimInstance.h"
-#include "SlashAnimInstance.generated.h"
-
-
-UCLASS()
-class MYPROJECT4_API USlashAnimInstance : public UAnimInstance
-{
-	GENERATED_BODY()
-public:
-	virtual void NativeinitializeAnimation() override;
-	virtual void NativeUpdateAnimation(float DeltaTime) override;
-
-	UPROPERTY(BlueprintReadOnly)
-	class AslashCharacter* SlashCharacter;
-
-	UPROPERTY(BlueprintReadOnly, Category = Movement)
-	class UCharacterMovementComponent* SlashCharacterMovement;
-
-	UPROPERTY(BlueprintReadOnly, Category=Movement)
-	float GroundSpeed;
-};
-
+//
+//
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "Animation/AnimInstance.h"
+//#include "SlashAnimInstance.generated.h"
+//
+//
+//UCLASS()
+//class MYPROJECT4_API USlashAnimInstance : public UAnimInstance
+//{
+//	GENERATED_BODY()
+//public:
+//	virtual void NativeinitializeAnimation() override;
+//	virtual void NativeUpdateAnimation(float DeltaTime) override;
+//
+//	UPROPERTY(BlueprintReadOnly)
+//	class AslashCharacter* SlashCharacter;
+//
+//	UPROPERTY(BlueprintReadOnly, Category = Movement)
+//	class UCharacterMovementComponent* SlashCharacterMovement;
+//
+//	UPROPERTY(BlueprintReadOnly, Category=Movement)
+//	float GroundSpeed;
+//};
+//
